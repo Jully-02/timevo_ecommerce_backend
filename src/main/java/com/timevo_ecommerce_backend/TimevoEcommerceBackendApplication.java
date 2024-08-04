f4398d815231f8dfa6d4814a60ba6960a0b2bba8
Nguyễn Đức Thiệp <nguyenducthiep16@gmail.com>
2024-08-04 14:50:46 +0700

    init project

---
+++ src/main/java/com/timevo_ecommerce_backend/TimevoEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.timevo_ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimevoEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimevoEcommerceBackendApplication.class, args);
+	}
+
+}
